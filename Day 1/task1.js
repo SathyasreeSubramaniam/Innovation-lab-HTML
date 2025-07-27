a4ff4818c36dd8f5f3aee1d97ef88ead0fa5dc57
Sathyasree Subramaniam <sathyasreesubramaniam25@gmail.com>
2025-07-27 23:06:41 +0530

    Add files via upload

---
+++ Day 1/task1.js
@@ -0,0 +1,12 @@
+var name = "Sathyasree";
+let age = 19;
+const country = "India";
+
+// Outputting messages to console
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Country:", country);
+
+// Modifying variables and observing output
+age = 20;
+console.log("Updated Age:", age);
